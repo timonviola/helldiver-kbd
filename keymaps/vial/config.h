c233fc3773b0bc1d79868789e1a8619cf5a72e34
timonviola <44016238+timonviola@users.noreply.github.com>
2024-04-13 14:16:01 +0200

    feat(wip): add vial support

---
+++ keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x07, 0x0A, 0x56, 0x0B, 0x17, 0xA3, 0x78, 0xCE}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 1}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+// reduce EEPROM usage
+#define DYNAMIC_KEYMAP_LAYER_COUNT 1
